e13d3170e7e1c7af11b735758d18ee0a1763a3e2
jspc <james@zero-internet.org.uk>
2015-12-23 13:40:27 +0000

    Create a super-simple javascripty find and replacer

---
+++ js/content.js
@@ -0,0 +1,7 @@
+console.log('Dean Swift Extension: starting');
+
+document.body.innerHTML = document.body.innerHTML.replace(
+    new RegExp("dean swift", "ig"), "Hand Shank"
+);
+
+console.log('Dean Swift Extension: done');
